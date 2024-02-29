fc34d53d15b7a28390215bf8be05512d2e62e454
davidsaav3 <davidsaav3@gmail.com>
2024-02-29 22:49:00 +0100

    mejora login y cambio

--- project/api/code/controllers/Users.js
+++ project/api/code/controllers/Users.js
@@ -7,6 +7,8 @@ router.use(express.json())
 const jwt = require('jsonwebtoken');
 const verifyToken = require('./token');
 const SECRET_KEY = process.env.TOKEN;
+const saltRounds = 10; // Adjust based on security requirements
+const bcrypt = require('bcrypt');
 
   router.get("/get/:type/:type1/:type2/:pag_tam/:pag_pag", verifyToken, (req, res) => {  /*/ GET  /*/
     const type0 = req.params.type;
@@ -31,31 +33,51 @@ const SECRET_KEY = process.env.TOKEN;
     });
   });
 
-  router.post("/login", (req, res) => {
-    const { email, password } = req.body;
-      if (!email || !password) {
-      return res.status(400).json({ error: 'El email y la contraseña son requeridos' });
-    }
+  router.post("/login", (req, res) => { // LOGIN
+      const { email, password } = req.body;
   
-    const query = "SELECT * FROM users WHERE email = ? AND password = ?";
-    con.query(query, [email, password], (err, result) => {
-      if (err) {
-        return res.status(500).json({ error: 'Error en la base de datos' });
+      if (!email || !password) {
+          return res.status(400).json({ error: 'El email y la contraseña son requeridos' });
       }
-      if (result.length === 1) {
-        const user = result[0];
-        const token = jwt.sign({ email: user.email }, SECRET_KEY);
-
-        return res.status(200).json({ id: user.id, email: user.email, token: token, message: 'Inicio de sesión exitoso' });
-      } 
-      else {
-        return res.status(401).json({ error: 'Credenciales incorrectas' });
-      }
-    });
+  
+      const query = "SELECT * FROM users WHERE email = ?";
+      con.query(query, [email], (err, result) => {
+          if (err) {
+              return res.status(500).json({ error: 'Error en la base de datos' });
+          }
+          if (result.length === 1) {
+              const user = result[0];
+              console.log("Contraseña cifrada almacenada:", user.password);
+              // Compara la contraseña introducida con la contraseña cifrada almacenada
+              bcrypt.compare(password, user.password, (bcryptErr, bcryptResult) => {
+                  if (bcryptErr) {
+                      console.error("Error al comparar contraseñas:", bcryptErr);
+                      return res.status(500).json({ error: 'Error al comparar contraseñas' });
+                  }
+                  console.log("Contraseña introducida:", password);
+                  console.log("Contraseña coincidente:", bcryptResult);
+                  if (bcryptResult) {
+                      // Si las contraseñas coinciden, genera y devuelve el token JWT
+                      const token = jwt.sign({ email: user.email }, SECRET_KEY);
+                      return res.status(200).json({
+                          id: user.id,
+                          email: user.email,
+                          token: token,
+                          message: 'Inicio de sesión exitoso'
+                      });
+                  } else {
+                      console.warn("Credenciales incorrectas");
+                      return res.status(401).json({ error: 'Credenciales incorrectas' });
+                  }
+              });
+          } else {
+              console.warn("Usuario no encontrado");
+              return res.status(401).json({ error: 'Credenciales incorrectas' });
+          }
+      });
   });
   
-
-
+  
   router.get("/id/:id", verifyToken, (req, res) => {  /*/ ID  /*/
     const id = parseInt(req.params.id);
     const query = "SELECT * FROM users WHERE id = ?";
@@ -92,25 +114,35 @@ const SECRET_KEY = process.env.TOKEN;
     });
   });
 
-  router.post("", verifyToken, (req, res) => {  /*/ POST  /*/
-    const { email, password, change_password } = req.body;
-    
-    if (!email || !password) {
-      return res.status(400).json({ error: 'Email y password  son requeridas' });
-    }
 
-    const query = "INSERT INTO users (email, password, change_password) VALUES (?, ?, ?)";
-    con.query(query, [email, password, change_password], (err, result) => {
+  router.post("", verifyToken, (req, res) => {  /*/ POST  /*/
+  const { email, password, change_password } = req.body;
+  
+  if (!email || !password) {
+    return res.status(400).json({ error: 'Email y password  son requeridas' });
+  }
+  console.log("Lo q me llega:", password);
+
+  // Genera un hash de la contraseña
+  bcrypt.hash(password, 10, (err, hashedPassword) => {
       if (err) {
-        return res.status(500).json({ error: 'Error en la base de datos' });
+          return res.status(500).json({ error: 'Error al cifrar la contraseña' });
       }
-      if (result.affectedRows === 1) {
-        const insertedId = result.insertId; // Obtiene el ID insertado
-        return res.status(201).json({ id: insertedId }); // Devuelve el ID en la respuesta
-      }
-      return res.status(500).json({ error: 'No se pudo insertar el registro' });
-    });
+      console.log("Cifrada:", hashedPassword);
+
+      const query = "INSERT INTO users (email, password, change_password) VALUES (?, ?, ?)";
+      con.query(query, [email, hashedPassword, change_password], (err, result) => {
+          if (err) {
+              return res.status(500).json({ error: 'Error en la base de datos' });
+          }
+          if (result.affectedRows === 1) {
+              const insertedId = result.insertId; // Obtiene el ID insertado
+              return res.status(201).json({ id: insertedId }); // Devuelve el ID en la respuesta
+          }
+          return res.status(500).json({ error: 'No se pudo insertar el registro' });
+      });
   });
+});
     
   router.put("/email", verifyToken, (req, res) => {  // UPDATE EMAIL
     const { id, email } = req.body;
@@ -134,68 +166,101 @@ const SECRET_KEY = process.env.TOKEN;
   });
 
   router.put("", (req, res) => {  /*/ UPDATE  /*/
-  const { id, email, password, change_password } = req.body;
-  if (!id || (!email && !password)) {
-    return res.status(400).json({ error: 'Se requiere el ID del usuario y al menos un campo para actualizar' });
-  }
-  let query = "UPDATE users SET";
-  const values = [];
-  if (email) {
-    query += " email=?";
-    values.push(email);
-  }
-  if (password) {
-    query += ", password=?";
-    values.push(password);
-  }
-  if (change_password!=null) {
-    query += ", change_password=?";
-    values.push(change_password);
-  }
-  query += " WHERE id=?";
-  values.push(id);
-  con.query(query, values, (err, result) => {
-    if (err) {
-      return res.status(500).json({ error: 'Error en la base de datos' });
-    }
-    if (result.affectedRows > 0) {
-      return res.status(200).json({ message: 'Registro actualizado con éxito' });
-    }
-      return res.status(404).json({ error: 'Registro no encontrado' });
+      const { id, email, password, change_password } = req.body;
+      if (!id || (!email && !password)) {
+          return res.status(400).json({ error: 'Se requiere el ID del usuario y al menos un campo para actualizar' });
+      }
+      let query = "UPDATE users SET";
+      const values = [];
+      if (email) {
+          query += " email=?";
+          values.push(email);
+      }
+      if (password) {
+          // Aplicar bcrypt.hash para cifrar la contraseña antes de almacenarla
+          bcrypt.hash(password, 10, (err, hashedPassword) => {
+              if (err) {
+                  return res.status(500).json({ error: 'Error al cifrar la contraseña' });
+              }
+              query += ", password=?";
+              values.push(hashedPassword); // Usar la contraseña cifrada
+              // Continuar con la ejecución de la consulta después de cifrar la contraseña
+              continueUpdateQuery();
+          });
+      } else {
+          // Si no se proporciona una nueva contraseña, continuar sin cifrarla
+          continueUpdateQuery();
+      }
+  
+      function continueUpdateQuery() {
+          if (change_password != null) {
+              query += ", change_password=?";
+              values.push(change_password);
+          }
+          query += " WHERE id=?";
+          values.push(id);
+          
+          con.query(query, values, (err, result) => {
+              if (err) {
+                  return res.status(500).json({ error: 'Error en la base de datos' });
+              }
+              if (result.affectedRows > 0) {
+                  return res.status(200).json({ message: 'Registro actualizado con éxito' });
+              }
+              return res.status(404).json({ error: 'Registro no encontrado' });
+          });
+      }
   });
-});
-
-
+  
   router.put("/password", verifyToken, (req, res) => {  // UPDATE PASSWORD
     const { id, password, newpassword1, newpassword2 } = req.body;
 
     if (!id || !password || !newpassword1 || !newpassword2) {
-      return res.status(400).json({ error: 'Todos los campos son requeridos' });
+        return res.status(400).json({ error: 'Todos los campos son requeridos' });
     }
     if (newpassword1 !== newpassword2) {
        return res.status(400).json({ error: 'Las nuevas contraseñas no coinciden' });
     }
 
-    const query = "SELECT * FROM users WHERE id = ? AND password = ?";
-    //console.log([id, password])
-    con.query(query, [id, password], (err, result) => {
-      if (err) {
-        return res.status(500).json({ error: 'Error en la base de datos' });
-      }
-      if (result.length === 0) {
-        return res.status(404).json({ error: 'Usuario no encontrado o contraseña antigua incorrecta' });
-      }
-
-      const updateQuery = "UPDATE users SET password = ? WHERE id = ?";
-      //console.log(req.body)
-      con.query(updateQuery, [newpassword1, id], (updateErr, updateResult) => {
-        if (updateErr) {
-          return res.status(500).json({ error: 'Error al actualizar la contraseña' });
+    // Buscar el usuario en la base de datos
+    const query = "SELECT * FROM users WHERE id = ?";
+    con.query(query, [id], (err, result) => {
+        if (err) {
+            return res.status(500).json({ error: 'Error en la base de datos' });
         }
-        return res.status(200).json({ message: 'Contraseña actualizada con éxito' });
-      });
+        if (result.length === 0) {
+            return res.status(404).json({ error: 'Usuario no encontrado' });
+        }
+
+        // Comparar la contraseña antigua con la contraseña almacenada
+        bcrypt.compare(password, result[0].password, (compareErr, compareResult) => {
+            if (compareErr) {
+                return res.status(500).json({ error: 'Error al comparar contraseñas' });
+            }
+            if (!compareResult) {
+                return res.status(400).json({ error: 'Contraseña antigua incorrecta' });
+            }
+
+            // Cifrar la nueva contraseña
+            bcrypt.hash(newpassword1, 10, (hashErr, hashedPassword) => {
+                if (hashErr) {
+                    return res.status(500).json({ error: 'Error al cifrar la nueva contraseña' });
+                }
+
+                const updateQuery = "UPDATE users SET password = ? WHERE id = ?";
+                con.query(updateQuery, [hashedPassword, id], (updateErr, updateResult) => {
+                    if (updateErr) {
+                        return res.status(500).json({ error: 'Error al actualizar la contraseña' });
+                    }
+                    return res.status(200).json({ message: 'Contraseña actualizada con éxito' });
+                });
+            });
+        });
     });
 });
+
+  
+  
 
 router.delete("", verifyToken, (req, res) => {  /*/ DELETE  /*/
   const id = parseInt(req.body.id);
