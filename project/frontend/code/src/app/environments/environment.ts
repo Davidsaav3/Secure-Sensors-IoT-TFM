992f7cbae9ea2b804477e9b5c5a208d83464d350
davidsaav3 <davidsaav3@gmail.com>
2024-05-12 09:39:11 +0200

    .

--- project/frontend/code/src/app/environments/environment.ts
+++ project/frontend/code/src/app/environments/environment.ts
@@ -3,16 +3,51 @@ export const environment = {
     languageLang: ["es", "en"],
     resultsPerPag: [10, 15, 50, 100],
     accessTokenMap: 'pk.eyJ1IjoiZGF2aWRzYWF2MyIsImEiOiJjbGl1cmZ4NG8wMTZqM2ZwNW1pcW85bGo4In0.ye1F3KfhnRZruosNYoAYYQ',
-    deviceConfigurations: '/device_configurations',
-    sensorsTypes: '/sensors_types',
-    dataStructure: '/data_structure',
-    variableDataStructure: '/variable_data_structure',
     defaultMapsStyle: 'streets-v12',
-    AppVersion: '1.0.7',
+    AppVersion: '2.6.0',
+
+    url: {
+        deviceConfigurations: '/device_configurations',
+        sensorsTypes: '/sensors_types',
+        dataStructure: '/data_structure',
+        variableDataStructure: '/variable_data_structure',
+        users: '/users',
+        conecctionRead: '/conecction_read',
+        conecctionWrite: '/conecction_write',
+        script: '/script',
+        monitoring: '/monitoring',
+    },
+
+    rute: {
+        deviceConfigurations: 'devices',
+        sensorsTypes: 'sensors',
+        dataStructure: 'structure',
+        variableDataStructure: 'variable-structure',
+        login: 'login',
+        users: 'users',
+        conecctionRead: 'conecction-read',
+        conecctionWrite: 'conecction-write',
+        script: 'script',
+        monitoring: 'monitoring',
+    },
 
     // PRODUCCIÓN //
     //baseUrl: "http://localhost/api",
 
     // DESARROLLO //
     baseUrl: "http://localhost:5172/api",
+
+    password_pattern: /^(?=.*[0-9])(?=.*[!@#$%^&*])(?=.*[A-Z]).{8,}$/,
+    title: 'Sensors IoT',
+
+    acces_token_timeout: 5000, // Tiempa hasta lanzar otra vez la consulta
+    script_status_timeout: 5000,
+
+    acces_token_times: 2,
+    script_status_times: 2,
+
+    script_status_frontend: 11000,
+    acces_token_frontend: 5000,
+
+    verbose: false,
 };
